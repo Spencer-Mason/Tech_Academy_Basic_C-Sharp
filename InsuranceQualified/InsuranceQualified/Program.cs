5bf1eb4a683126addb0095a422d55a28461bf1eb
Spencer Mason <spencer.d.mason19@gmail.com>
2025-02-27 16:44:42 -0800

    Add files via upload

---
+++ InsuranceQualified/InsuranceQualified/Program.cs
@@ -0,0 +1,10 @@
+﻿// This program determines if a person is qualified for car insurance based on their age and driving record.
+Console.WriteLine("What is your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Have you ever had a DUI? Please answer 'true' or 'false'.");
+bool dui = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("How many speeding tickets do you have?");
+int tickets = Convert.ToInt32(Console.ReadLine());
+bool qualified = age > 15 && !dui && tickets <= 3;
+Console.WriteLine("Qualified?");
+Console.WriteLine(qualified);
