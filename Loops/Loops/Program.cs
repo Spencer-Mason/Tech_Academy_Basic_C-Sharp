341c4a0b4c512b603842616a20f8c338816b7d0f
Spencer Mason <spencer.d.mason19@gmail.com>
2025-02-28 12:33:01 -0800

    Add files via upload

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿bool end = false;
+
+while(!end)
+{
+    Console.WriteLine("Guess a number from 1 to 50:");
+    int guess = Convert.ToInt32(Console.ReadLine());
+    bool correct = guess == 42;
+    do
+    {
+        switch (guess)
+        {
+            case 42:
+                Console.WriteLine("That's right! GG.");
+                correct = true;
+                break;
+            default:
+                Console.WriteLine("You guessed " + guess + ". Sorry, that's wrong. Try again.");
+                guess = Convert.ToInt32(Console.ReadLine());
+                break;
+        }
+    }
+    while (!correct);
+    end = true;
+}
