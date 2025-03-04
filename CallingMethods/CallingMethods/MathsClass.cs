8d5e4666d6f6e263887318e9dbd8d27f49c424bf
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-04 09:05:28 -0800

    Add files via upload

---
+++ CallingMethods/CallingMethods/MathsClass.cs
@@ -0,0 +1,25 @@
+﻿namespace CallingMethods
+{
+    //Create class
+    class MathsClass
+    {
+        // Method to add 5 to user's number, with 'userNum' as a parameter
+        public static int Adding(int userNum)
+        {
+            int numberAdded = userNum + 5;
+            return numberAdded;
+        }
+        // Method to multiply by two. Same parameter as adding method
+        public static int Multiply(int userNum)
+        {
+            int numberMult = userNum * 2;
+            return numberMult;
+        }
+        // Dividing method. In this example we see that you don't need to create a variable.
+        // You can just return the math equation.
+        public static int Divide(int userNum)
+        {
+            return userNum / 2;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿// Calling the namespace to access the class within
+using CallingMethods;
+
+// Ask user to input number
+Console.WriteLine("Please enter a number to do some math on.");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// Call method within writeline, passing it user's input as an argument, to display it's return
+Console.WriteLine("Your number plus five is: " + MathsClass.Adding(userNum));
+Console.WriteLine("Your number multiplied by 2 is: " + MathsClass.Multiply(userNum));
+Console.WriteLine("Your number divided by 2 is: " + MathsClass.Divide(userNum));
