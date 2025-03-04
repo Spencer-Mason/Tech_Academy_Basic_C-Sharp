d9d667782a693149212f50c193174f9f0b1cb6d3
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-04 14:07:19 -0800

    Add files via upload

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,13 @@
+﻿using MethodOverloading;
+
+class Program
+{
+    static void Main()
+    {
+        // Changing which method is being used by
+        // passing in different data types
+        Console.WriteLine(MathsClass.Math(20)); // int
+        Console.WriteLine(MathsClass.Math(50.54)); // double
+        Console.WriteLine(MathsClass.Math("10")); // string
+    }
+}
