56b0a830254ee084b95e4c8096818b7d8316cb07
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-04 19:22:09 -0800

    Add files via upload

---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Class1
+    {
+        public void Method1(int paramOne, int paramTwo)
+        {
+            int mathStuff = paramOne * 5;
+            Console.WriteLine("First number times 5: {0}", mathStuff);
+            Console.WriteLine("Second number: {0}", paramTwo);
+        }
+    }
+}
---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,7 @@
+﻿using MethodClassSubmissionAssignment;
+
+// Instantiate class
+Class1 cl = new();
+
+// Calling the method and specifying the parameters by name
+cl.Method1(paramOne: 5, paramTwo: 12);
