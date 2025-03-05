4eb03112bc5c6b8f1b873e5f4816bb9273042649
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-05 10:00:14 -0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        // Method with an output parameter, marked as void because there's no return value
+        public void Method1(int param1, out int variable)
+        {
+            variable =  param1 / 2;
+        }
+
+        // Overloaded method to take two variables instead of one
+        public void Method1(int param1, int param2, out int variable)
+        {
+            variable = param1 / param2;
+        }
+    }
+
+    // A static class
+    static class Class2
+    {
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+
+// Instantiate class
+Class1 class1 = new();
+
+// Ask user to input a number to be divided by two
+Console.WriteLine("Please enter a number to divide it by 2:");
+// Convert user input from string to int to be used in math operations
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+// Declare result variable to store the output from the method
+int result;
+// Call method, passing in user input, outputing the result
+class1.Method1(userNumber, out result);
+
+// Display result in console
+Console.WriteLine("Your number divided by two: " + result);
