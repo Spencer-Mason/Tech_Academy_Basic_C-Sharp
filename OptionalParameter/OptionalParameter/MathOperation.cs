0c697c70fbfbcbdd4cfe239a717cf25aa6152ee4
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-04 18:35:53 -0800

    Add files via upload

---
+++ OptionalParameter/OptionalParameter/MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class MathOperation
+    {
+        public int MathMethod(int x, int y = 2)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,24 @@
+﻿using OptionalParameter;
+
+// Instantiate class
+MathOperation math = new ();
+
+// Ask user for first number input
+Console.WriteLine("Enter the first number for a math operation");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+// Ask user for optional second number
+Console.WriteLine("Enter the second number, or just press enter to use default number 2");
+
+// If user entered a number, save it as secondNum and print operation result
+if (int.TryParse(Console.ReadLine(), out int secondNum))
+{
+    int result = math.MathMethod(firstNum, secondNum);
+    Console.WriteLine(firstNum + " times " + secondNum + " is: " + result);
+}
+// If user didn't enter a number, default number is used and print operation result
+else
+{
+    int result = math.MathMethod(firstNum);
+    Console.WriteLine(firstNum + " times 2 is: " + result);
+}
