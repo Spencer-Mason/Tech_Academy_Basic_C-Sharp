6677a8c7db6cec68126f647ccf5a228abeed2d79
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-05 17:07:30 -0800

    Add files via upload

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Employee class inhetits from abstract class Person
+    class Employee : Person
+    {
+        // Override method to alter it and print name to console
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,7 @@
+﻿using AbstractClass;
+
+// Instantiate class and set name values
+Employee employee = new() { FirstName = "Sample", LastName = "Student" };
+
+// Calling method to print name to console
+employee.SayName();
