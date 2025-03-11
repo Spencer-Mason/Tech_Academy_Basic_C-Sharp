460df43e68a6f53356be53b7a7c74ba69c535b8b
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-11 11:32:04 -0700

    Add files via upload

--- MethodsAndObjects/MethodsAndObjects/Employee.cs
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace MethodsAndObjects
 {
-    // Class that inherits from person class
-    public class Employee : Person
+    // Class that inherits from person class, and implements IQuittable
+    public class Employee : Person, IQuittable
     {
         // Adds an ID property
         public int Id { get; set; }
+
+        // Implement the Quit method
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} has quit.", FirstName, LastName);
+        }
     }
 }
---
+++ MethodsAndObjects/MethodsAndObjects/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    // Define interface with a Quit method
+    // This ensures that any class inheriting from IQuittable must define the Quit() method
+    interface IQuittable
+    {
+        void Quit();
+    }
+}
