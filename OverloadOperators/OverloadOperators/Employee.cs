8c6263789de126f658ff4201cb58333af48a4137
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-11 17:43:53 -0700

    Add files via upload

---
+++ OverloadOperators/OverloadOperators/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    public class Employee
+    {
+        // Define employee properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overriding the '==' operator
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+            if (emp1 is null || emp2 is null)
+                return false;
+            return emp1.Id == emp2.Id;
+        }
+        // Overriding '!=' operator; they must be done in pairs
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        // Overriding Equals and GetHashCode is good practice when overloading the '==' operator
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee other)
+                return this.Id == other.Id;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OverloadOperators/OverloadOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿using OverloadOperators;
+
+// Creating employee objects
+Employee emp1 = new Employee { Id = 1, FirstName = "Spencer", LastName = "Mason" };
+Employee emp2 = new Employee { Id = 1, FirstName = "Mos", LastName = "Mason" };
+Employee emp3 = new Employee { Id = 3, FirstName = "Tenebris", LastName = "Cat" };
+
+// Compare employees using overloaded operators
+// Equal to
+Console.WriteLine("Employee one and two have the same ID? " + (emp1 == emp2));
+Console.WriteLine("Employee one and three have the same ID? " + (emp1 == emp3));
+// Not equal to
+Console.WriteLine("Employee one and two have different IDs? " + (emp1 != emp2));
+Console.WriteLine("Employee two and three have different IDs? " + (emp2 != emp3));
