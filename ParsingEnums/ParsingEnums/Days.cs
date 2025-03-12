bc50a460ec51b1cfcebd45de31dfaeabe2d96904
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-12 12:44:18 -0700

    Add files via upload

---
+++ ParsingEnums/ParsingEnums/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class Days
+    {
+        // Create enum of days of the week
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,19 @@
+﻿using ParsingEnums;
+
+// Ask user for day of the week
+Console.WriteLine("Please enter a day of the week:");
+string day = Console.ReadLine();
+
+// Using a try/catch block in case user input is invalid
+try
+{
+    // Make sure just the first letter of user input is capitalized
+    day = char.ToUpper(day[0]) + day.Substring(1).ToLower();
+    Days.DaysOfWeek parseDay = (Days.DaysOfWeek)Enum.Parse(typeof(Days.DaysOfWeek), day);
+    Console.WriteLine("You entered: " + parseDay);
+}
+catch (Exception)
+{
+    // Error message if any ecxeptions are thrown
+    Console.WriteLine("Please enter an actual day of the week.");
+}
