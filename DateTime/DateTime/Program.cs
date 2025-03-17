65a1b594559c051480f4b2032eca1955c1e909ea
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-17 11:40:13 -0700

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,8 @@
+﻿// First print the current date/time
+Console.WriteLine("The current date/time is: " + DateTime.Now);
+// Ask user to input a number of hours
+Console.WriteLine("Please enter a number of hours to add to the current time:");
+// Convert user input to an integer and store in a variable
+int hours = Convert.ToInt32(Console.ReadLine());
+// Add number of hours to current date/time and print result
+Console.WriteLine("The date/time in {0} hour will be: {1}", hours, DateTime.Now.AddHours(hours));
