32d8d07afe9755ace0738f20fb2933a2b0520c79
Spencer Mason <spencer.d.mason19@gmail.com>
2025-03-17 18:04:24 -0700

    Add files via upload

---
+++ ConstructorChain/ConstructorChain/Class1.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConstructorChain
+{
+    public class Class1
+    {
+        // First constructor just takes number input and passes default name and multiplier
+        public Class1(int number) : this("Guest User", number, 2) { }
+        // Next two take either a name and number or a number and multiplier and pass defaults accordingly
+        public Class1(string name, int number) : this(name, number, 2) { }
+        public Class1(int number, int mult) : this("Guest User", number, mult) { }
+        // Lastly we take all arguments, defualt or user input, and produce a result
+        public Class1(string name, int number, int mult)
+        {
+            int result = number * mult;
+            Console.WriteLine("Hello, {0}, here is your result: {1}", name, result);
+        }
+    }
+}
---
+++ ConstructorChain/ConstructorChain/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConstructorChain;
+
+// Create and print a constant greeting variable
+const string greeting = "Welcome to the Multiply Two Numbers Application!";
+Console.WriteLine(greeting);
+
+// Ask for user name input optionally
+Console.WriteLine("Enter your name, or don't.");
+var name = Console.ReadLine();
+// Ask for a number to multiply
+Console.WriteLine("Enter a number to multiply(required)");
+// Define an int variable for TryParse method
+int number;
+// Keeps asking for input as long as user input is not a valid number
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Please enter a valid number:");
+}
+// Ask for an optional second number to multiply
+Console.WriteLine("Enter another number, or press enter to use default 2");
+var input = Console.ReadLine();
+// Define the mult variable for another TryParse
+int mult;
+// If TryParse is succeessful, update mult value with user input and set hasMult to true
+// If not successful, hasMult is false and mult is not used
+bool hasMult = int.TryParse(input, out mult);
+
+// Ternary operator with nested ternary operators to determine what arguments are passed to the class
+// based on if user input a name and/or a multiplier
+Class1 user = string.IsNullOrWhiteSpace(name)
+    ? (hasMult ? new(number, mult) : new(number))
+    : (hasMult ? new(name, number, mult) : new(number));
